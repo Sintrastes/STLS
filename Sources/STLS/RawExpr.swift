b71c3e22545c1288d7ea9f7d2eb0d243489b725f
Nathan Bedell <nbedell@tulane.edu>
2022-07-17 15:22:25 -0400

    Initial implementation of RawExpr.

---
+++ Sources/STLS/RawExpr.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nathan Bedell on 7/17/22.
+//
+
+import Foundation
+
+indirect enum RawExpr: Codable {
+    case const(value: String)
+    case lam(identifier: String, body: RawExpr)
+    case app(f: RawExpr, x: RawExpr)
+    case variable(identifier: String)
+    case customOp(identifier: String)
+}
