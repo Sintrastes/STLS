ad2f4f8f4367f962dcf191229c6d07d53b51c5ab
Nathan Bedell <nbedell@tulane.edu>
2022-07-17 16:34:16 -0400

    Add stub TypeUtils file.

---
+++ Sources/STLS/TypeUtils.swift
@@ -0,0 +1,10 @@
+//
+//  TypeUtils.swift
+//
+//  TODO: Utilities for manipulating and inspecting types of Swift objects
+//    at runtime.
+//
+//  Created by Nathan Bedell on 7/17/22.
+//
+
+import Foundation
