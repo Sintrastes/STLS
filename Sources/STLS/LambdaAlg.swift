9dc1cc2e46da27aada073ca285d1fa925b7ca90f
Nathan Bedell <nbedell@tulane.edu>
2022-07-17 15:54:43 -0400

    Use the bow versions of functors rather than rolling our own.

--- Sources/STLS/LambdaAlg.swift
+++ Sources/STLS/LambdaAlg.swift
@@ -13,13 +13,29 @@ struct LambdaAlgInterpreter: LambdaAlg {
     
     static func fun<A, B>(_ f: @escaping (Kind<ForId, A>) -> Kind<ForId, B>) -> Kind<ForId, (A) -> B> {
         return Id { (x: A) in
-            return f(Id(value: x)).fix()
+            return f(Id(x))^.value
         }
     }
     
     static func apply<A, B>(_ f: Kind<ForId, (A) -> B>, _ x: Kind<ForId, A>) -> Kind<ForId, B> {
-        return Id(
-            value: f.fix()(x.fix())
+        return Id(f^.value(x^.value))
+    }
+}
+
+struct LambdaAlgSerializer: LambdaAlg {
+    typealias F = ConstPartial<RawExpr>
+    
+    static func fun<A, B>(_ f: @escaping (ConstOf<RawExpr, A>) -> ConstOf<RawExpr, B>) -> ConstOf<RawExpr, (A) -> B> {
+        return Const(
+            f(
+                Const(RawExpr.variable(identifier: "x"))
+            )^.value
+        )
+    }
+    
+    static func apply<A, B>(_ expr1: ConstOf<RawExpr, (A) -> B>, _ expr2: ConstOf<RawExpr, A>) -> ConstOf<RawExpr, B> {
+        return Const(
+            RawExpr.app(f: expr1^.value, x: expr2^.value)
         )
     }
 }
