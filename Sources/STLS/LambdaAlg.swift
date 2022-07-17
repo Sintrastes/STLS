3856f1170cf362f5be86dedfa97e11d2512db768
Nathan Bedell <nbedell@tulane.edu>
2022-07-17 15:16:05 -0400

    Add LambdaAlgInterpreter.

--- Sources/STLS/LambdaAlg.swift
+++ Sources/STLS/LambdaAlg.swift
@@ -7,3 +7,19 @@ protocol LambdaAlg {
     static func fun<A, B>(_: @escaping (Kind<F, A>) -> Kind<F, B>) -> Kind<F, (A) -> B>
     static func apply<A, B>(_: Kind<F, (A) -> B>, _: Kind<F,A>) -> Kind<F, B>
 }
+
+struct LambdaAlgInterpreter: LambdaAlg {
+    typealias F = ForId
+    
+    static func fun<A, B>(_ f: @escaping (Kind<ForId, A>) -> Kind<ForId, B>) -> Kind<ForId, (A) -> B> {
+        return Id { (x: A) in
+            return f(Id(value: x)).fix()
+        }
+    }
+    
+    static func apply<A, B>(_ f: Kind<ForId, (A) -> B>, _ x: Kind<ForId, A>) -> Kind<ForId, B> {
+        return Id(
+            value: f.fix()(x.fix())
+        )
+    }
+}
